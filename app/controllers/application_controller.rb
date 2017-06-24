df23023db1e0e13f483893600e202d68bcead981
Duy Phuc <phuc.tran@yomi.studio>
2017-06-24 23:02:01 +0700

    Create mesages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :require_login, except: [:new,:create]
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
+  end
+  def require_login
+    if session.has_key?(:user_id)
+
+    else
+      redirect_to new_session_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  acts_as_reader
   validates :email, presence: true, uniqueness: true
+
 end
