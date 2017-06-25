3b99867a6661294a04e520850b2b5c1919ac7c12
Duy Phuc <phuc.tran@yomi.studio>
2017-06-25 23:36:38 +0700

    Handle read message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   get 'index' => 'home#index'
   delete 'log_out' => 'sessions#destroy'
+  get 'sent_messages' => 'messages#sent_messages'
   resources :users
   resources :friendships, only: [:create,:destroy]
   resources :sessions, only: [:new, :create,:destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624165655) do
+ActiveRecord::Schema.define(version: 20170625084456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170624165655) do
     t.integer "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "readed_at"
   end
 
   create_table "read_marks", id: :serial, force: :cascade do |t|
